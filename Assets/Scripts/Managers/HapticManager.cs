e5157976308f1f326a095c4972eb3b5327e788ee
Siddu Eswar <siddueshu@gmail.com>
2022-07-28 22:03:35 +0530

    Handheld vibration is working

--- Assets/Scripts/Managers/HapticManager.cs
+++ Assets/Scripts/Managers/HapticManager.cs
@@ -6,29 +6,33 @@ public class HapticManager : MonoBehaviour
 {
 	[SerializeField] private Settings settings;
 	private static HapticManager _this;
-	private AndroidJavaObject vibrator;
 
 	private const int SHORT_TIME = 100;
 	private const int MEDIUM_TIME = 250;
 	private const int LONG_TIME = 500;
 
+	private AndroidJavaClass unityPlayer;
+	private AndroidJavaObject currentActivity;
+	private AndroidJavaObject vibrator;
+
+
 	private void Awake()
 	{
 		_this = this;
-
-		/*
-		if (isAndroid()){
-			AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-			AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-			vibrator = currentActivity.Call<AndroidJavaObject>("getSystemSErvice", "vibrator");
-		}
-		*/
-		
+#if UNITY_ANDROID && !UNITY_EDITOR
+    unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+    currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+    vibrator = currentActivity.Call<AndroidJavaObject>("getSystemService", "vibrator");
+#endif
 	}
 
 	private bool isAndroid()
 	{
-		return true; // TODO: Check the condition of android
+#if UNITY_ANDROID && !UNITY_EDITOR
+		return true;
+#else
+		return false;
+#endif
 	}
 
 	public static void ShortVibration()
@@ -51,15 +55,17 @@ public class HapticManager : MonoBehaviour
 	{
 		if (settings.haptic)
 		{
+			/*
 			if (isAndroid())
 			{
-				Debug.Log("vibrating:" + milliseconds);
-				//vibrator.Call("vibrate", milliseconds); // milliseconds
+				vibrator.Call("vibrate", milliseconds);
 			}
 			else
 			{
+				*/
+
 				Handheld.Vibrate();
-			}
+			//}
 		}
 	}
 }
